b4a1dcb88099e643f38304a16ee24ac9f6e2bcac
rtmtorresmorales <ratorres@ponce.inter.edu>
2023-02-24 04:41:33 -0400

    Update emailer2.py

--- Optional/emailer2.py
+++ Optional/emailer2.py
@@ -2,8 +2,6 @@
 Send an email with Python.email
 Original Author: Denise Case
 Date: 2022-12-27
-Editor/Student: DeeDee Walker
-Date: 2/19/23
 We'll need an outgoing email service -
 this example uses a gmail account.
 uses:
